2ebc90e0e0f99d69b81ce87bf43f9767668c7576
miguel.ochoa <miguel.ochoa.gallegos@gmail.com>
2019-02-13 17:50:26 -0500

    carga fuentes

---
+++ backend/src/main/resources/public/precache-manifest.df679640dcc60a3c9358abe897d9e8ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ec7b0018301743ed2cc3",
+    "url": "/static/js/main.ec7b0018.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "ec7b0018301743ed2cc3",
+    "url": "/static/css/main.8fcbff88.chunk.css"
+  },
+  {
+    "revision": "69f72b3f9aaa19d57b833f234e94b57b",
+    "url": "/index.html"
+  }
+];
